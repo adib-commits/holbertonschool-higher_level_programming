5c3938a86419c23e525a5dac0a1ef909cde36592
adib-commits <hdadib442@gmail.com>
2026-05-28 13:53:39 +0200

    That serializes and deserialize custom Python objects using the pickle module

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python3
-import json
+import pickle
 
 
-def serialize_and_save_to_file(data, filename):
-    with open(filename, "w") as f:
-        json.dump(data, f)
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
 
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
-def load_and_deserialize(filename):
-    with open(filename, "r") as f:
-        return json.load(f)
+    def serialize(self, filename):
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except Exception:
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except Exception:
+            return None
