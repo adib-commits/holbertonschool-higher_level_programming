14a7ede7ba311da771b7f3bcf3fcd7b848a00680
adib-commits <hdadib442@gmail.com>
2026-06-11 12:44:13 +0000

    sys.argv out of the db

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,12 +8,15 @@ import sys
 
 
 if __name__ == "__main__":
+    user=sys.argv[1],
+    password=sys.argv[2],
+    db_name=sys.argv[3]
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
+        user=user,
+        passwrd=password,
+        db=db_name
     )
 
     cur = db.cursor()
