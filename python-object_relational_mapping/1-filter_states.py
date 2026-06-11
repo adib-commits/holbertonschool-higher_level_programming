045f6eb63c6b003262c5f42679012a89b1e18732
adib-commits <hdadib442@gmail.com>
2026-06-11 12:39:44 +0000

    That lists all states with a name starting with N from the database hbtn_0e_0_usa

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
 
     cur.execute(
         "SELECT * FROM states "
-        "WHERE name LIKE 'N%' "
+        "WHERE name LIKE BINARY 'N%' "
         "ORDER BY id ASC"
     )
 
