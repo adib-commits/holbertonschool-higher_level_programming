9262be50bd3093e3038cd7d2183f2893a6af58cb
adib-commits <hdadib442@gmail.com>
2026-07-02 08:54:34 +0000

    That searches the second biggest integer in the list of arguments

---
+++ javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2).map(Number);
+
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  let max = Math.max(...args);
+  let second = -Infinity;
+
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > second && args[i] < max) {
+      second = args[i];
+    }
+  }
+
+  console.log(second);
+}
