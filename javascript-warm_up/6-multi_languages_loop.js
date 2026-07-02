56d0771c12ff2be3aa011ca8952325be29502081
adib-commits <hdadib442@gmail.com>
2026-07-02 09:16:52 +0000

    that prints 3 lines by using an array of string and a loop

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
 
-const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const languages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
 
-let output = '';
-let i = 0;
-
-while (i < languages.length) {
-  output += languages[i];
-    output += '\n';
-
-  i++;
-}
-
-console.log(output);
+console.log(languages.join('\n'));
