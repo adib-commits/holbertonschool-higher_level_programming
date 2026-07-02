eb1d59cc0fcffc9105bb3bf2cef8c4948b3430cc
adib-commits <hdadib442@gmail.com>
2026-07-02 14:26:51 +0000

    That prints the addition of 2 integers

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 function add (a, b) {
-    return a + b;
+  return a + b;
 }
-  
-  const x = parseInt(process.argv[2]);
-  const y = parseInt(process.argv[3]);
-  
-  console.log(add(x, y));
+
+const x = parseInt(process.argv[2]);
+const y = parseInt(process.argv[3]);
+
+console.log(add(x, y));
