b2a91f4c35e54b242d563d38481f7eb8d5f5fc46
adib-commits <hdadib442@gmail.com>
2026-08-13 08:50:03 +0000

    added new element

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
-"""
-Lists all State objects from the database.
-"""
+"""Lists all State objects from a MySQL database."""
 
-import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+import sys
 
 
 if __name__ == "__main__":
     engine = create_engine(
         "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]
+            sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True
     )
